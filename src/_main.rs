762692d858443de23f621575b134b8dc79d7dca4
wartyz <zwenderk@hotmail.com>
2023-03-20 19:29:40 +0100

    Acabado Capítulo 2

---
+++ src/_main.rs
@@ -0,0 +1,102 @@
+// https://www.youtube.com/watch?v=TQt-v_bFdao
+
+
+use bevy::prelude::*;
+use crate::Trabajo::{Abogado, Doctor};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PeoplePlugin)
+        .run();
+}
+
+pub struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_startup_system(setup)
+            .add_system(print_names)
+            .add_system(gente_con_trabajo)
+            .add_system(gente_sin_trabajo)
+            .add_system(gente_y_su_trabajo);
+    }
+}
+
+pub fn setup(mut commands: Commands) {
+    println!("hola");
+    // Si es más de un Component, hay que ponerlos en una tupla
+    commands.spawn(
+        (
+            Empleo {
+                trabajo: Doctor,
+            },
+            Person {
+                name: "Luis".to_string(),
+            })
+    );
+
+    // No tiene Component "Empleado"
+    commands.spawn(Person {
+        name: "Pedro".to_string(),
+    });
+
+    commands.spawn(
+        (
+            Person {
+                name: "Jose".to_string(),
+            },
+            Empleo {
+                trabajo: Abogado,
+            })
+    );
+}
+
+pub fn print_names(person_query: Query<&Person>) {
+    for person in person_query.iter() {
+        dbg!(&person.name);
+    }
+}
+
+pub fn gente_con_trabajo(
+    person_query: Query<&Person, With<Empleo>>,
+) {
+    for person in person_query.iter() {
+        println!("\nTienen trabajo -> {}", person.name)
+    }
+}
+
+pub fn gente_sin_trabajo(
+    person_query: Query<&Person, Without<Empleo>>,
+) {
+    for person in person_query.iter() {
+        println!("\nNo Tienen trabajo -> {}", person.name)
+    }
+}
+
+// Si hacemos una query a más de un Component, hay que ponerlos en una tupla
+pub fn gente_y_su_trabajo(
+    person_query: Query<(&Person, &Empleo)>
+) {
+    for (person, empleo) in person_query.iter() {
+        println!("\nLas personas y su trabajo son trabajo -> {:?} y {:?}", person.name, empleo.trabajo)
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Person {
+    pub name: String,
+}
+
+#[derive(Component, Debug)]
+pub struct Empleo {
+    pub trabajo: Trabajo,
+}
+
+#[derive(Debug)]
+pub enum Trabajo {
+    Doctor,
+    Oficinista,
+    Abogado,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,48 @@
-// https://www.youtube.com/watch?v=TQt-v_bFdao
+// https://www.youtube.com/watch?v=izhFutJiZgo&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&index=2
 
+use std::thread::spawn;
 use bevy::prelude::*;
-use crate::Trabajo::{Abogado, Doctor};
+use bevy::window::PrimaryWindow;
 
-fn main() {
+pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(PeoplePlugin)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
         .run();
 }
 
-pub struct PeoplePlugin;
+#[derive(Component)]
+pub struct Player {}
 
-impl Plugin for PeoplePlugin {
-    fn build(&self, app: &mut App) {
-        app
-            .add_startup_system(setup)
-            .add_system(print_names)
-            .add_system(gente_con_trabajo)
-            .add_system(gente_sin_trabajo)
-            .add_system(gente_y_su_trabajo);
-    }
-}
-
-pub fn setup(mut commands: Commands) {
-    println!("hola");
-    // Si es más de un Component, hay que ponerlos en una tupla
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
     commands.spawn(
         (
-            Empleo {
-                trabajo: Doctor,
+            SpriteBundle {
+                transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.),
+                texture: asset_server.load("sprites/ball_blue_large.png"),
+                ..default()
             },
-            Person {
-                name: "Luis".to_string(),
-            })
-    );
-
-    // No tiene Component "Empleado"
-    commands.spawn(Person {
-        name: "Pedro".to_string(),
-    });
-
-    commands.spawn(
-        (
-            Person {
-                name: "Jose".to_string(),
-            },
-            Empleo {
-                trabajo: Abogado,
-            })
+            Player {}
+        )
     );
 }
 
-pub fn print_names(person_query: Query<&Person>) {
-    for person in person_query.iter() {
-        dbg!(&person.name);
-    }
-}
-
-pub fn gente_con_trabajo(
-    person_query: Query<&Person, With<Empleo>>,
+pub fn spawn_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
-    for person in person_query.iter() {
-        println!("\nTienen trabajo -> {}", person.name)
-    }
-}
+    let window = window_query.get_single().unwrap();
 
-pub fn gente_sin_trabajo(
-    person_query: Query<&Person, Without<Empleo>>,
-) {
-    for person in person_query.iter() {
-        println!("\nNo Tienen trabajo -> {}", person.name)
-    }
-}
-
-// Si hacemos una query a más de un Component, hay que ponerlos en una tupla
-pub fn gente_y_su_trabajo(
-    person_query: Query<(&Person, &Empleo)>
-) {
-    for (person, empleo) in person_query.iter() {
-        println!("\nLas personas y su trabajo son trabajo -> {:?} y {:?}", person.name, empleo.trabajo)
-    }
-}
-
-#[derive(Component, Debug)]
-pub struct Person {
-    pub name: String,
-}
-
-#[derive(Component, Debug)]
-pub struct Empleo {
-    pub trabajo: Trabajo,
-}
-
-#[derive(Debug)]
-pub enum Trabajo {
-    Doctor,
-    Oficinista,
-    Abogado,
+    commands.spawn(
+        Camera2dBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.),
+            ..default()
+        }
+    );
 }
