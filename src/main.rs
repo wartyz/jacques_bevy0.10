2b2e15ff41d4f801167ad8064474d16a218ea564
wartyz <zwenderk1@hotmail.com>
2023-03-26 20:14:18 +0200

    Capítulo 3   Final

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 // https://www.youtube.com/watch?v=izhFutJiZgo&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&index=2
 
-use std::thread::spawn;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use rand::prelude::*;
 
 pub const PLAYER_SIZE: f32 = 64.;
 // Es el tamaño del sprite del Player
 pub const PLAYER_SPEED: f32 = 500.;
+pub const NUMBER_OF_ENEMIES: usize = 4;
 
 pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_enemies)
         .add_system(player_movement)
         .add_system(confine_player_movement)
         .run();
@@ -20,6 +22,9 @@ pub fn main() {
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -51,6 +56,28 @@ pub fn spawn_camera(
             ..default()
         }
     );
+}
+
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let random_x: f32 = random::<f32>() * window.width();
+        let random_y: f32 = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
+    }
 }
 
 pub fn player_movement(
